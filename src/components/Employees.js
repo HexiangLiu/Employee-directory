12195b38ff13667f0294d4a0d07c9d908cb81073
Henry Liu <hl3184@columbia.edu>
2020-08-28 19:02:51 -0400

    sorting age and filtering country complete

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -5,16 +5,45 @@ import axios from 'axios';
 class Employees extends React.Component {
   state = {
     employees: [],
+    ascending: false,
+    filterdEmployees: [],
   };
   componentDidMount() {
     axios
-      .get('https://randomuser.me/api/?page=3&results=50')
+      .get('https://randomuser.me/api/?page=3&results=80')
       .then((res) => {
-        console.log(res);
-        this.setState({ employees: res.data.results });
+        this.setState({
+          employees: res.data.results,
+          filterdEmployees: res.data.results,
+        });
       })
       .catch((err) => console.log(err));
   }
+
+  getCountriesList(employees) {
+    const set = new Set(employees.map((employee) => employee.location.country));
+    return [...set];
+  }
+
+  handleFiltering = (e) => {
+    if (e.target.value === 'All') {
+      this.setState({ filterdEmployees: this.state.employees });
+    } else {
+      this.setState({
+        filterdEmployees: this.state.employees.filter(
+          (employee) => employee.location.country === e.target.value
+        ),
+      });
+    }
+  };
+  handleSorting = () => {
+    this.setState((state) => ({ ascending: !state.ascending }));
+    this.setState((state) => ({
+      filterdEmployees: state.filterdEmployees.sort((a, b) =>
+        state.ascending ? a.dob.age - b.dob.age : b.dob.age - a.dob.age
+      ),
+    }));
+  };
 
   render() {
     return (
@@ -23,13 +52,27 @@ class Employees extends React.Component {
           <tr>
             <th>Profile</th>
             <th>Name</th>
-            <th>Age</th>
+            <th>
+              Age <i className="fas fa-sort" onClick={this.handleSorting}></i>
+            </th>
             <th>Email</th>
-            <th>Country</th>
+            <th>
+              Country <i className="fas fa-filter"></i>
+              <select onChange={this.handleFiltering}>
+                <option value="All">All</option>
+                {this.getCountriesList(this.state.employees).map(
+                  (country, index) => (
+                    <option key={index} value={country}>
+                      {country}
+                    </option>
+                  )
+                )}
+              </select>
+            </th>
           </tr>
         </thead>
         <tbody>
-          {this.state.employees.map((employee) => (
+          {this.state.filterdEmployees.map((employee) => (
             <Employee employee={employee} key={employee.login.uuid} />
           ))}
         </tbody>
